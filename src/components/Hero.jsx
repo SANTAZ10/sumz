4d965cda36da714d62f602da012aaf1213bd593e
samuel <fagbolasamuel.sf@gmail.com>
2024-01-09 19:27:54 +0100

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex pt-3 mb-10 w-full justify-between items-center">
+        <img src={logo} alt="sumz logo" className="w-28 object-contain" />
+        <button className="black_btn" type="button" onClick={() => window.open('https://github.com/SANTAZ10/ai_summarizerls')}>
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Sumarize Articles with <br className="max-md:hidden"/>
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc">
+      Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
